978690a9a95c4cc6ea6714b498099bbdc3ad5ea9
damienrabelais <damien.boucard@ac-rennes.fr>
2024-09-16 15:10:07 +0200

    Exercice 72, G1

---
+++ corrections_tps_prog/exercice72G1.vb
@@ -0,0 +1,26 @@
+﻿Module exercice72G1
+    Sub main()
+
+        Dim jours, distance As Integer
+        Dim coutTotalEssence, coutTotalDiesel As Double
+
+        Console.WriteLine("Nombre de jours de location ?")
+        jours = Console.ReadLine()
+        Console.WriteLine("Distance à parcourir (en kms) ?")
+        distance = Console.ReadLine()
+
+        coutTotalEssence = jours * 30 + 0.85 * distance
+        coutTotalDiesel = jours * 35 + 0.65 * distance
+
+        If coutTotalEssence > coutTotalDiesel Then
+            Console.WriteLine("Meilleur choix : Diesel")
+        ElseIf coutTotalEssence = coutTotalDiesel Then
+            Console.WriteLine("Egalité")
+        Else
+            Console.WriteLine("Meilleur choix : Essence")
+        End If
+
+        Console.ReadLine()
+    End Sub
+
+End Module
