fa317978da37e24c90ef5d6aaabaf5248b1fe110
damienrabelais <damien.boucard@ac-rennes.fr>
2024-10-07 14:10:22 +0200

    Ajout corrections Ex. 9.3 et 9.4 (boucles)

---
+++ corrections_tps_prog/exercice93.vb
@@ -0,0 +1,29 @@
+﻿Module exercice93
+    Sub Main()
+        Dim n, i, nombre As Integer
+
+        Do
+            Console.WriteLine("n ? (n > 0)")
+            n = Console.ReadLine()
+            If n <= 0 Then
+                Console.WriteLine("n > 0")
+            End If
+        Loop Until n > 0
+
+        ' SOLUTION 1
+        'nombre = 0
+        'For i = 1 To n
+        '    nombre = nombre + 2
+        '    Console.WriteLine("nombre : " + nombre.ToString())
+        'Next
+
+        ' SOLUTION 2
+        For nombre = 2 To n * 2 Step 2
+            Console.WriteLine("nombre : " + nombre.ToString())
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ corrections_tps_prog/exercice94.vb
@@ -0,0 +1,39 @@
+﻿Module exercice94
+    Sub Main()
+        Const TAUXCOMMISSION As Double = 0.1
+        Dim cARepresentant, ventesHTHebdo As Double
+        Dim i As Integer
+        Dim choix, nomRepresentant As String
+        Do
+            Console.WriteLine("Nom du représentant ?")
+            nomRepresentant = Console.ReadLine
+            cARepresentant = 0
+            For i = 1 To 4
+                Console.WriteLine("Semaine " + i.ToString() +
+                ": Ventes H.T hebdomadaires (0 pour stopper la saisie) ?")
+                Do
+                    ventesHTHebdo = Console.ReadLine
+                    If ventesHTHebdo < 0 Then
+                        Console.WriteLine("Ventes ne peuvent être < 0")
+                    End If
+                Loop Until ventesHTHebdo >= 0
+                If ventesHTHebdo = 0 Then
+                    Exit For
+                End If
+                cARepresentant = cARepresentant + ventesHTHebdo ' Cumul des ventes
+            Next
+            Console.WriteLine("Bilan pour " + nomRepresentant)
+            Console.WriteLine("Total des ventes H.T. = " +
+                      cARepresentant.ToString())
+            Console.WriteLine("Commission = " +
+                      (TAUXCOMMISSION * cARepresentant).ToString())
+            Do ' Saisie avec vérification
+                Console.WriteLine("Autre représentant(O/N)")
+                choix = Console.ReadLine
+            Loop Until choix = "o" Or choix = "O" Or choix = "n" Or choix = "N"
+        Loop Until choix = "N" Or choix = "n"
+        Console.WriteLine("Au revoir.")
+        Console.ReadLine()
+    End Sub
+
+End Module
