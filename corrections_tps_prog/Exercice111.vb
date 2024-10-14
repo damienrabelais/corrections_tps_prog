49804827be24dfa01c7d3d8cb92a70a4a5ea7335
damienrabelais <damien.boucard@ac-rennes.fr>
2024-10-14 16:25:34 +0200

    Correction 11.1 V2

--- corrections_tps_prog/Exercice111.vb
+++ corrections_tps_prog/Exercice111.vb
@@ -1,22 +1,22 @@
 ﻿Module Exercice111
 
-    Function périmètreCercle(ByVal pRayon As Double) As Double
+    Function PérimètreCercle(ByVal pRayon As Double) As Double
         Dim périmètre As Double  'périmètre est une variable locale à la fonction
         périmètre = 2 * Math.PI * pRayon
         Return périmètre ' valeur retournée
     End Function
 
-    Function surfaceCercle(ByVal pRayon As Double) As Double
+    Function SurfaceCercle(ByVal pRayon As Double) As Double
         Dim surface As Double 'surface est une variable locale à la fonction
         surface = Math.PI * pRayon ^ 2
         Return surface ' valeur retournée
     End Function
 
-    Function périmètreRectangle(ByVal pLongueur As Double, ByVal pLargeur As Double) As Double
+    Function PérimètreRectangle(ByVal pLongueur As Double, ByVal pLargeur As Double) As Double
         Return (pLongueur + pLargeur) * 2 ' valeur retournée
     End Function
 
-    Function surfaceRectangle(ByVal pLongueur As Double, ByVal pLargeur As Double) As Double
+    Function SurfaceRectangle(ByVal pLongueur As Double, ByVal pLargeur As Double) As Double
         Return (pLongueur * pLargeur) ' valeur retournée
     End Function
 
@@ -42,14 +42,18 @@
                             Console.WriteLine("Rayon > 0")
                         End If
                     Loop Until rayonSaisi > 0
-                    Console.WriteLine("Périmètre: " + périmètreCercle(rayonSaisi).ToString())
+                    Console.WriteLine("Périmètre: " + PérimètreCercle(rayonSaisi).ToString())
                 Case 2
-                    Console.WriteLine("Rayon du cercle ? (Rayon > 0) ")
-                    rayonSaisi = Console.ReadLine()
-                    If rayonSaisi <= 0 Then
-                        Console.WriteLine("Rayon > 0")
-                    End If
-                    Console.WriteLine("Surface : " + surfaceCercle(rayonSaisi).ToString())
+                    Do
+                        Console.WriteLine("Rayon du cercle ? (Rayon > 0) ")
+                        rayonSaisi = Console.ReadLine()
+                        If rayonSaisi <= 0 Then
+                            Console.WriteLine("Rayon > 0")
+                        End If
+                    Loop Until rayonSaisi > 0
+                    Dim surfaceRetournée As Double
+                    surfaceRetournée = SurfaceCercle(rayonSaisi)
+                    Console.WriteLine("Surface : " + surfaceRetournée.ToString())
                 Case 3
                     Do
                         Console.WriteLine("Longueur du rectangle (Longueur > 0) ")
@@ -66,7 +70,7 @@
                         End If
                     Loop Until largeurSaisie > 0
                     Console.WriteLine("Périmètre: " +
-                    périmètreRectangle(longueurSaisie, largeurSaisie).ToString())
+                    PérimètreRectangle(longueurSaisie, largeurSaisie).ToString())
                 Case 4
                     Do
                         Console.WriteLine("Longueur du rectangle (Longueur > 0) ")
@@ -83,7 +87,7 @@
                         End If
                     Loop Until largeurSaisie > 0
                     Console.WriteLine("Surface : " +
-                    surfaceRectangle(longueurSaisie, largeurSaisie).ToString())
+                    SurfaceRectangle(longueurSaisie, largeurSaisie).ToString())
                 Case 5
                     Console.WriteLine("Au revoir")
                 Case Else
