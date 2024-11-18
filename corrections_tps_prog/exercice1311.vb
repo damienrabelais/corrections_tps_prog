8fd0eb758115cad5ea9823b8dfc2fa403714ca21
damienrabelais <damien.boucard@ac-rennes.fr>
2024-11-18 16:42:26 +0100

    Exercices Tableaux 1, 2

---
+++ corrections_tps_prog/exercice1311.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Net.Mail
+
+Module exercice1311
+
+
+    Sub main()
+        Dim noMois As Integer
+
+        Dim mois() As String = {"Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+            "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"}
+
+        Do
+            Console.WriteLine("Numéro mois ? (1-12)")
+            noMois = Console.ReadLine()
+            If noMois > 12 Or noMois < 1 Then
+                Console.WriteLine("Le numéro du mois doit être compris entre 1 et 12)")
+            End If
+        Loop Until noMois <= 12 And noMois >= 1
+
+
+        Console.WriteLine("Libellé du mois :" + mois(noMois - 1))
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ corrections_tps_prog/exercice1312.vb
@@ -0,0 +1,32 @@
+﻿Module exercice1312
+    Sub MinMax(ByVal pTab() As Double)
+        Dim leMax, leMin As Double
+        Dim i As Integer
+        leMax = pTab(0)
+        leMin = pTab(0)
+
+        For i = 1 To 9
+            If pTab(i) > leMax Then
+                leMax = pTab(i)
+            End If
+            If pTab(i) < leMin Then
+                leMin = pTab(i)
+            End If
+        Next
+        Console.WriteLine("Min : " + leMin.ToString())
+        Console.WriteLine("Max : " + leMax.ToString())
+    End Sub
+
+    Sub Main()
+        Dim lesNombres(9) As Double
+        Dim i As Integer
+        For i = 0 To 9
+            Console.WriteLine("Nombre n°" + i.ToString() + " ?")
+            lesNombres(i) = Console.ReadLine()
+        Next
+        MinMax(lesNombres)
+
+        Console.ReadLine()
+    End Sub
+
+End Module
